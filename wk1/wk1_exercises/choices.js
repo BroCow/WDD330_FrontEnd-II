ad667f84cc02b06d83a7d072131db6b6f4bd8796
Christopher Cowan <cow18003@byui.edu>
2021-04-22 20:21:07 -0700

    week1

--- wk1/wk1_exercises/choices.js
+++ wk1/wk1_exercises/choices.js
@@ -2,10 +2,22 @@
 
 function checkNumber(){
     var numberEntered = document.getElementById("number").value;
-    var remainder = numberEntered % 2;
-    if(remainder == 0){
-        document.getElementById("result").innerHTML = "The number " + numberEntered + " is even!";
-    } else {
-        document.getElementById("result").innerHTML = "The number " + numberEntered + " is odd!";
+    var numberVerified = parseInt(numberEntered);
+
+    if(numberEntered != "" && !isNaN(numberVerified)){
+        var remainder = Math.abs(numberVerified % 2);
+        if(numberEntered != 0 && remainder == 0){
+            document.getElementById("result").innerHTML = "The number " + numberEntered + " is even!";
+        } 
+        else if(numberEntered != 0 && remainder == 1){
+            document.getElementById("result").innerHTML = "The number " + numberEntered + " is odd!";
+        }
+        else{
+            document.getElementById("result").innerHTML = "The number entered is 0, so it is neither even or odd!";
+        }
     }
-}
+    else{
+        document.getElementById("result").innerHTML = "Please enter a number!";
+    }
+
+} 
